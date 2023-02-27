f06f556b6b36f471cf2f84887a700d51c1f9a592
LautyFran <126511753+LautyFran@users.noreply.github.com>
2023-02-27 16:17:24 -0300

    Add files via upload

---
+++ animacion.js
@@ -0,0 +1,21 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function menuMostrarOcultar(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//el menu se olculta al selecionar una opcion 
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
